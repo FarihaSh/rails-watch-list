cd5b074a13790edf858eca410f92f09b6ce7348c
Fariha Shah fariha@shahinvest.co <fariha@shahinvest.co>
2022-08-19 11:30:50 +0200

    correction

--- db/schema.rb
+++ db/schema.rb
@@ -50,12 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_124604) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "boomarks", force: :cascade do |t|
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
