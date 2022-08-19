6ab22b7d2f165853f65a8b2606e1afafafefd472
Fariha Shah fariha@shahinvest.co <fariha@shahinvest.co>
2022-08-19 11:01:02 +0200

    setup

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,9 @@
 class List < ApplicationRecord
-  has_many :movies, through: :bookmarks
+  has_many :movies, through: :bookmark
   has_many :bookmarks, dependent: :destroy
   has_one_attached :photo
 
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, on: :create
   validates :photo, presence: true, allow_blank: false
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
   has_many  :bookmarks, dependent: :destroy
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
+  validates :title, length: { minimum: 6 }
 end
